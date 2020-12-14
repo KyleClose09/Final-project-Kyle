40c08070f1ddd65a5f0f973845d5151ae00f4d0b
KyleClose09 <kclose@ualberta.ca>
2020-12-13 19:44:39 -0700

    All assets added and placed besides the roof and lights requested by ally for marking

---
+++ Assets/Blender materials/readmefile1160.cs
@@ -0,0 +1,15 @@
+﻿
+//this is just a short explaination of what is in this scene at this time
+//the objects that are hare to see are the keys and radio. 
+//key one is in the vent above the toilet
+//key two is under the grate by the broken robot
+//radio is behind the mirror.
+//if time alows i will dress up the ajecent room
+//will add a roof same color as the flooring
+//isnt there due to easier editing process 
+//will add after sound and light
+//overall this is a good feel start for me id probably explore more real world texures if given more time
+//kept things as simple as possible got far more confortable with more expirence but i do feel
+//from anart point of view i could do more 
+//thank you ally
+//kyle
